ff6df71f5f9f38cd1fdb4b70979dc6692fc57d26
RustyRaptor <rustyraptor@users.noreply.github.com>
2018-01-04 21:02:35 -0700

    Changed variable names and reformatted.
    Signed-off-by: RustyRaptor <ziadarafat1@gmail.com>

--- Notify.py
+++ Notify.py
@@ -5,18 +5,19 @@ from datetime import datetime
 HIGH = 1
 LOW = 0
 
+
 class Notify(threading.Thread):
-    ''' Notify class. Responsible for managing notifications time management
+    """ Notify class. Responsible for managing notifications time management
     
         Low priority breaks don't affect how long you need to wait for another
         break. 
         
         High priority breaks reset the time needed for the next break.
-    '''
-    
-    def __init__(self, interval = 10,
-                duration = 5, name = None,
-                priority = LOW):
+    """
+
+    def __init__(self, interval=10,
+                 duration=5, name=None,
+                 priority=LOW):
         # class constructor
         threading.Thread.__init__(self)
         self.event = threading.Event()
@@ -28,21 +29,21 @@ class Notify(threading.Thread):
 
     def notify(self):
         # add notification procedures here
-        print ( str(datetime.now()) 
-                + " - Time to: " + self.name 
-                + " - Duration: " + str(self.duration))
-        
+        print(str(datetime.now())
+              + " - Time to: " + self.name
+              + " - Duration: " + str(self.duration))
+
     def run(self):
         # main thread method
         self.counter(self.interval)
-        self.notify() 
+        self.notify()
         if self.priority == HIGH:
-            for notificator in notificators:
-                notificator.stop()
+            for notifier in notifiers:
+                notifier.stop()
         self.counter(self.duration)
         if self.priority == HIGH:
-            for notificator in notificators:
-                notificator.run()
+            for notifier in notifiers:
+                notifier.run()
         else:
             self.run()
 
@@ -51,8 +52,12 @@ class Notify(threading.Thread):
         while number > 0 and not self.event.is_set():
             number -= 1
             self.event.wait(1)
-    
+
     def stop(self):
         # this method pauses the thread
         self.event.set()
 
+
+testbreak = Notify(10, 5, "ay", HIGH)
+testbreak.start()
+notifiers = [testbreak]
